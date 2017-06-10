a4a755b10714d80d63c63348c8fb89900fcd1481
Edward Bramanti <edward@bramanti.org>
2017-06-09 21:40:26 -0700

    Add css-modules rules

---
+++ rules/plugins/css-modules.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "plugins": [
+        "css-modules"
+    ],
+    "rules": [
+        'css-modules/no-unused-class': "error",
+        'css-modules/no-undef-class': "error",
+    ]
+}
