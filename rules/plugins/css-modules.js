1d5fdc643d7a35b35bd7b698006e0172d01ddfda
Edward Bramanti <edward@bramanti.org>
2017-06-10 19:23:16 -0700

    Linting rule files

--- rules/plugins/css-modules.js
+++ rules/plugins/css-modules.js
@@ -1,9 +1,9 @@
 module.exports = {
     "plugins": [
-        "css-modules"
+        "css-modules",
     ],
-    "rules": [
-        'css-modules/no-unused-class': "error",
-        'css-modules/no-undef-class': "error",
-    ]
-}
+    "rules": {
+        "css-modules/no-unused-class": "error",
+        "css-modules/no-undef-class": "error",
+    },
+};
