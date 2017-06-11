89c812bbbcdbfa867a157c45f82953813dad404c
Edward Bramanti <edward@bramanti.org>
2017-06-10 19:43:24 -0700

    Enable linting of self in project

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+const reactRules = [
+    "./rules/plugins/react",
+    "./rules/plugins/react-a11y",
+    "./rules/plugins/css-modules",
+].map(require.resolve);
+
 module.exports = {
-    "extends": [
-        "videoamp",
-        "./rules/plugins/react",
-        "./rules/plugins/react-a11y",
-        "./rules/plugins/css-modules",
-    ].map(require.resolve),
+    "extends": ["videoamp"].concat(reactRules),
 };
