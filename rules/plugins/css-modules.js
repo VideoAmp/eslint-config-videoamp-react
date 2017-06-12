640114bacecdae3529702936902a15f6d9d0d5c9
Edward Bramanti <edward@bramanti.org>
2017-06-12 15:18:12 -0700

    Cleanup

--- rules/plugins/css-modules.js
+++ rules/plugins/css-modules.js
@@ -1,7 +1,5 @@
 module.exports = {
-    "plugins": [
-        "css-modules",
-    ],
+    "plugins": ["css-modules"],
     "rules": {
         "css-modules/no-unused-class": "error",
         "css-modules/no-undef-class": "error",
