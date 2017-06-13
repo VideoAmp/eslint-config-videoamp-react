2e654a502f780e65d7225828b4d0e13826ae97d6
Edward Bramanti <edward@bramanti.org>
2017-06-12 17:00:05 -0700

    Add babel-eslint and parser options to config - Add `babel-eslint` to project - Add parsing options that correspond to project - Set env

--- index.js
+++ index.js
@@ -7,4 +7,21 @@ const reactRules = [
 
 module.exports = {
     "extends": ["videoamp"].concat(reactRules),
+    "parser": "babel-eslint",
+    "env": {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        jest: true,
+        node: true,
+    },
+    "parserOptions": {
+        ecmaVersion: 2017,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
+            generators: true,
+            experimentalObjectRestSpread: true,
+        },
+    },
 };
