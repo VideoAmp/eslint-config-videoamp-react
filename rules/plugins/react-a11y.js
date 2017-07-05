697c54687de3d5fe20cfacc2b5041bcc0ac8645b
Edward Bramanti <edward@bramanti.org>
2017-07-05 10:02:43 -0700

    Update rules for 6.0.2

--- rules/plugins/react-a11y.js
+++ rules/plugins/react-a11y.js
@@ -15,6 +15,10 @@ module.exports = {
         // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-has-content.md
         "jsx-a11y/anchor-has-content": ["error", { components: [""] }],
 
+        // Enforces that anchors navigate
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md
+        "jsx-a11y/anchor-is-valid": "error",
+
         // Require ARIA roles to be valid and non-abstract
         // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/aria-role.md
         "jsx-a11y/aria-role": ["error", { ignoreNonDom: false }],
@@ -31,10 +35,6 @@ module.exports = {
         // properties do not have those attributes.
         // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/aria-unsupported-elements.md
         "jsx-a11y/aria-unsupported-elements": "error",
-
-        // disallow href "#"
-        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/href-no-hash.md
-        "jsx-a11y/href-no-hash": ["error", { components: ["a"] }],
 
         // Enforce that all elements that require alternative text have meaningful information
         // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/alt-text.md
